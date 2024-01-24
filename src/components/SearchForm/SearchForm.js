49848a549bd8c49d91cc881858313d71dafb31ec
Rozovika Ivan <ivanrozovika@yandex.ru>
2024-01-25 01:00:30 +0300

    fix rev bem

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,19 +4,19 @@ import FilterCheckbox from "../FilterCheckbox/FilterCheckbox.js";
 function SearchForm() {
   return (
     <div className="search">
-      <section className="search search-content">
-        <form className="search-content__form" name="search-content">
+      <section className="search search__content">
+        <form className="search__form" name="search">
           <input
-            className="search-content__input"
+            className="search__input"
             name="search"
             type="text"
             placeholder="Фильм"
             required
             autoComplete="off"
           />
-          <button className="search-content__button" type="submit">Найти</button>
+          <button className="search__button" type="submit">Найти</button>
         </form>
-        <span className="search-content__grey-line"></span>
+        <span className="search__grey-line"></span>
         <FilterCheckbox />
       </section>
       <span className="search__bottom-line"></span>
